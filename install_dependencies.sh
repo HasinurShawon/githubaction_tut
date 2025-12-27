e668f6cc779206b5048a75bd98837facca19dd3d
HasinurShawon <hasinur.shawon@gmail.com>
2025-12-27 18:03:35 +0600

    pm2 install from bash

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-set -e
+set -e  # fail fast
 
-sudo apt update
+sudo apt update -y
 sudo apt install -y nginx
 sudo systemctl start nginx
+
+echo "Installing Node.js"
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt install -y nodejs
+
+sudo npm install -g pm2
+
+echo "Showing running PM2 processes..."
+pm2 list || echo "No PM2 processes running yet"
